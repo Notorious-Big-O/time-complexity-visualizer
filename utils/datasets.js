3fec1bed45081f27d997bcf873ac843d1203b6fb
Glen Jarvis <glen@glenjarvis.com>
2025-08-01 20:13:42 -0700

    feat(algo-testing): Add basic algo-testing framework

--- utils/datasets.js
+++ utils/datasets.js
@@ -5,6 +5,17 @@ const maxSize = 7; // Number of zeros (e.g., 10^8) (100,000,000)
 const maxInt = 1000; // Each item would be from 1 to 1,000
 
 /*
+ * Time how long a function takes to complete
+ */
+function timeFunction(fn, ...args) {
+  const start = Date.now();
+  const result = fn(...args);
+  const end = Date.now();
+
+  return (end - start).toFixed(4); // ms
+}
+
+/*
  * Return array of full filepaths for files that contain the datasets.
  */
 function datasetFileNames() {
@@ -51,3 +62,28 @@ function createRandomDatasets() {
     });
   });
 }
+
+/*
+ * Given a given function defition, call that function
+ * with increasingly large numbers of input data. Print
+ * time taken:
+ * n:      100     time: 1.0000
+ * n:     1000     time: 3.0000
+ * n:    10000     time: 70.0000
+ * n:   100000     time: 6418.0000
+ */
+function testAlgo(fn) {
+  datasetFileNames().forEach((filename) => {
+    const n = fetchRandomDataset(filename);
+    const time = timeFunction(fn, n);
+    console.log(`n: ${n.length.toString().padStart(8)}\ttime: ${time}`);
+  });
+}
+
+export {
+  timeFunction,
+  datasetFileNames,
+  fetchRandomDataset,
+  createRandomDatasets,
+  testAlgo
+};
