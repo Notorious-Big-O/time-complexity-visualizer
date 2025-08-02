47cc986295d4c34dc0e2cd312a53aa2b005c51fd
Laura <lauraschlueter@ymail.com>
2025-08-02 12:53:59 -0700

    feat(algo): add sortAndRemoveDup

---
+++ algo-snippets/sample_algo.js
@@ -0,0 +1,13 @@
+import { testAlgo } from "../utils/datasets.js";
+
+function myAlgo(n) {
+  let count = 0;
+  for (let i = 0; i < n.length; i++) {
+    for (let j = 0; j < n.length; j++) {
+      if (n[i] === n[j]) count++;
+    }
+  }
+  return count;
+}
+
+testAlgo(myAlgo);
