53503a015d4b9217aa5fc5700e33d1bec1d512e2
Laura <lauraschlueter@ymail.com>
2025-08-02 12:39:13 -0700

    feat(algo): add countEvens

---
+++ algo-snippets/algo-snippet-1.js
@@ -0,0 +1,11 @@
+import { testAlgo } from "../utils/datasets.js";
+
+function countEvens(n) {
+let count = 0;
+for(let i = 0; i < n.length; i++) {
+    if(n[i] % 2 === 0) count++;
+  }
+return count;
+};
+
+testAlgo(countEvens);
