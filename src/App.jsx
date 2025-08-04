ded7e15662b1641ca542c07c2f8fea3f96db8639
harleydi <disneyharley26@gmail.com>
2025-08-04 15:44:47 -0400

    feat(react): added graph images and deleted dead code

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Control from './components/Control'
 import Graph from './components/Graph'
 import CodePanel from './components/CodePanel'
 import { hardcodedData } from '../data'
+import bubbleSortImg from '../graph-images/bubbleSort.algo.png'
+import countEvensImg from '../graph-images/countEvens.algo.png'
+import sortAndRemoveDupImg from '../graph-images/sortAndRemoveDup.algo.png'
 
 function App() {
   const [algoSelect, setAlgoSelect] = useState(hardcodedData.bubbleSort)
-  
+  const [graphMatch, setGraphMatch] = useState()
+
+  console.log(algoSelect.algoFn.name === 'bubbleSort')
+
+  useEffect(() => {
+    if (algoSelect.algoFn.name === 'bubbleSort') {
+      setGraphMatch(bubbleSortImg)
+    } else if (algoSelect.algoFn.name === 'countEvens') {
+      setGraphMatch(countEvensImg)
+    } else if (algoSelect.algoFn.name === 'sortAndRemoveDup') {
+      setGraphMatch(sortAndRemoveDupImg)
+    } else {
+      setGraphMatch("Graph Not Found")
+    }
+  }, [algoSelect])
 
   return (
     <div className='app'>
       <h1>BIG-O CALCULATOR</h1>
       <div className='top'>
         <Control algoSelect={algoSelect} setAlgoSelect={setAlgoSelect} />
-        <Graph />
+        <Graph graphMatch={graphMatch} />
       </div>
       <div className='bottom'>
         <CodePanel algoSelect={algoSelect} />
