8cd69415b9201ae76f0622171a26315e248321af
Glen Jarvis <glen@glenjarvis.com>
2025-08-03 19:42:48 -0700

    feat(test-framework): Automate testing framework

---
+++ algo-snippets/countEvens.js
@@ -0,0 +1,9 @@
+function countEvens(n) {
+  let count = 0;
+  for (let i = 0; i < n.length; i++) {
+    if (n[i] % 2 === 0) count++;
+  }
+  return count;
+}
+
+export { countEvens };
---
+++ algo-snippets/sortAndRemoveDup.js
@@ -0,0 +1,13 @@
+function sortAndRemoveDup(n) {
+  const sorted = n.slice().sort((a, b) => a - b);
+  const result = [sorted[0]];
+
+  for (let i = 1; i < sorted.length; i++) {
+    if (sorted[i] !== sorted[i - 1]) {
+      result.push(sorted[i]);
+    }
+  }
+  return result;
+}
+
+export { sortAndRemoveDup };
