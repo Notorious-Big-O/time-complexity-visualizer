dfbfe2a9b6d00a5c03fd52c9b5836e33c4aad483
Aaron Chen <chenxiyue7@gmail.com>
2025-08-06 10:03:25 -0700

    feat(vitest): setup vitest and test bubbleSort with vitest from algo-snippets, should make it a different commit but change the format for findMax to findMax.js to fix file naming error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
+
+//make sure to install Vitest
+//npm install --save-dev vitest @testing-library/react @testing-library/jest-dom
+
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    // allow `describe`, `it`, `expect` globally
+    globals: true,
+    // make sure to install jsdom for npm i -D jsdom
+    // we can also just use happy-dom but we might use jsdom later for canvas           
+    environment: 'jsdom',    
+    //tell vitest to discover and run them as a test
+    include: ['src/**/*.{test,spec}.{js,jsx}','algo-snippets/**/*.{test,spec}.{js,jsx}'], 
+  },
+});
