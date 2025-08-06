3635116e5cdc91d12132cb784563f7ab9f0a3486
Aaron Chen <chenxiyue7@gmail.com>
2025-08-06 10:19:55 -0700

    fix(prompt):remove unnecessary prompt comment

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 
-//make sure to install Vitest
-//npm install --save-dev vitest @testing-library/react @testing-library/jest-dom
-
 export default defineConfig({
   plugins: [react()],
   test: {
     // allow `describe`, `it`, `expect` globally
     globals: true,
-    // make sure to install jsdom for npm i -D jsdom
     // we can also just use happy-dom but we might use jsdom later for canvas           
     environment: 'jsdom',    
     //tell vitest to discover and run them as a test
