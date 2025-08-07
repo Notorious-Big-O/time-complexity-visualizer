e942f44b906c7b494de2814c67df4b66ff2985df
Aaron Chen <chenxiyue7@gmail.com>
2025-08-07 14:18:41 -0700

    feat(vittest):implementing the coverage for vitest

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,25 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
-    // allow `describe`, `it`, `expect` globally, , but the injection only happens at runtime, so it is better for us to 
+    // allow `describe`, `it`, `expect` globally, , but the injection only happens at runtime, so it is better for us to
     // import them explictly for the ESLint to know about those globals. This is more like a pro
     globals: true,
-    // we can also just use happy-dom but we might use jsdom later for canvas           
-    environment: 'jsdom',    
+    //enabling coverage
+    coverage: {
+      provider: 'v8', 
+      reporter: ['text', 'lcov', 'html'],
+      reportsDirectory: './coverage',
+      all: true, 
+      exclude: ['**/*.test.*', 'node_modules/**'],
+    },
+    // we can also just use happy-dom but we might use jsdom later for canvas
+    environment: 'jsdom',
     //tell vitest to discover and run them as a test
-    include: ['src/**/*.{test,spec}.{js,jsx}','algo-snippets/**/*.{test,spec}.{js,jsx}','utils/**/*.{test,spec}.{js,jsx}','tests/**/*.test.js'],
+    include: [
+      'src/**/*.{test,spec}.{js,jsx}',
+      'algo-snippets/**/*.{test,spec}.{js,jsx}',
+      'utils/**/*.{test,spec}.{js,jsx}',
+      'tests/**/*.test.js'
+    ],
   },
 });
