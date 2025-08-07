15ce4873f8120d31b2abcbc449fb3388b65313d8
tuckerolsen <tuckerolsen23@gmail.com>
2025-08-07 08:27:53 -0700

    chore(style): Add initial radiobutton structure

--- src/components/Control.jsx
+++ src/components/Control.jsx
@@ -13,55 +13,75 @@ const Control = ({
   runTests,
 }) => {
   return (
-    <div className='control'>
-      <div>
-        <div className={styles.algoSection}>
-          <label htmlFor=''>Choose your Algorithm: </label>
+    <div className={styles.controlBox}>
+      <div className='control'>
+        <div>
+          <div className={styles.algoSection}>
+            <label htmlFor=''>Choose your Algorithm: </label>
 
-          <select
-            className={styles.algoSelectBox}
-            name=''
-            id=''
-            onChange={(e) => setAlgoSelect(hardcodedData[e.target.value])}
-          >
-            <option value='bubbleSort'>bubbleSort</option>
-            <option value='sortAndRemoveDup'>sortAndRemoveDup</option>
-            <option value='countEvens'>countEvens</option>
-          </select>
+            <select
+              className={styles.algoSelectBox}
+              name=''
+              id=''
+              onChange={(e) => setAlgoSelect(hardcodedData[e.target.value])}
+            >
+              <option value='bubbleSort'>bubbleSort</option>
+              <option value='sortAndRemoveDup'>sortAndRemoveDup</option>
+              <option value='countEvens'>countEvens</option>
+            </select>
+          </div>
+        </div>
+
+        <div className={styles.startStopIncrement}>
+          <label htmlFor=''>Starting # of Inputs: </label>
+          <input
+            className={styles.startingInput}
+            type='number'
+            id='starting'
+            value={startingN}
+            onChange={(e) => setStartingN(e.target.value)}
+          />
+
+          <label htmlFor=''>Ending # of Inputs: </label>
+          <input
+            className={styles.endingInput}
+            type='number'
+            id='ending'
+            value={endingN}
+            onChange={(e) => setEndingN(e.target.value)}
+          />
+
+          <label htmlFor=''>Increment By: </label>
+          <input
+            className={styles.incrementInput}
+            type='number'
+            id='skip'
+            value={resolution}
+            onChange={(e) => setResolution(e.target.value)}
+          />
+        </div>
+        <div>
+          <button onClick={runTests}>Start Test</button>
+          <button className={styles.abortButton}>Abort</button>
         </div>
       </div>
-
-      <div className={styles.startStopIncrement}>
-        <label htmlFor=''>Starting # of Inputs: </label>
-        <input
-          className={styles.startingInput}
-          type='number'
-          id='starting'
-          value={startingN}
-          onChange={(e) => setStartingN(e.target.value)}
-        />
-
-        <label htmlFor=''>Ending # of Inputs: </label>
-        <input
-          className={styles.endingInput}
-          type='number'
-          id='ending'
-          value={endingN}
-          onChange={(e) => setEndingN(e.target.value)}
-        />
-
-        <label htmlFor=''>Increment By: </label>
-        <input
-          className={styles.incrementInput}
-          type='number'
-          id='skip'
-          value={resolution}
-          onChange={(e) => setResolution(e.target.value)}
-        />
-      </div>
-      <div>
-        <button onClick={runTests}>Start Test</button>
-        <button className={styles.abortButton}>Abort</button>
+      <div class={styles.algoControls}>
+        <p>
+          <form>
+            <div>
+              <input type='radio' name='algoSelector' value='log(n)' />
+              <label htmlFor=''>O(log(n))</label>
+            </div>
+            <div>
+              <input type='radio' name='algoSelector' value='log(n)' />
+              <label htmlFor=''>O(n*log(n)) </label>
+            </div>
+            <div>
+              <input type='radio' name='algoSelector' value='log(n)' />
+              <label htmlFor=''>O(n) </label>
+            </div>
+          </form>
+        </p>
       </div>
     </div>
   );
