73ead14ef4b419b5b36570b9ab7f2d2a1a886fc4
Aaron Chen <chenxiyue7@gmail.com>
2025-08-07 08:24:34 -0700

    test(utils): test each metric data points to be what is expected according to the input value

--- utils/testing_framework.test.js
+++ utils/testing_framework.test.js
@@ -1,4 +1,4 @@
-import { testingParamsFactory, dataPointFactory } from './testing_framework.js';
+import { testingParamsFactory, dataPointFactory, timeAtN} from './testing_framework.js';
 import { describe, it, expect } from 'vitest';
 
 describe('testingParamsFactory()', () => {
@@ -14,12 +14,26 @@ describe('testingParamsFactory()', () => {
 });
 
 describe('dataPointFactory()', () => {
-  it('creates an object with all the right keys and they should be initialized as undefined', () => {
+  it('creates an object with all the right metric keys and they should be initialized as undefined', () => {
     const dp = dataPointFactory();
     expect(Object.keys(dp)).toEqual([
       'numberOfInputs','algoDatapoint','exponential',
       'n_qubed','n_squared','n','n_log_n','log_n'
     ]);
     Object.values(dp).forEach(v => expect(v).toBeUndefined());
+  });
+});
+
+describe('timeAtN()', () => {
+  it('test the metric data points with avgN=1, n=2, measured=3', () => {
+    const dp = timeAtN(1, 2, 3);
+    expect(dp.numberOfInputs).toBe(2);
+    expect(dp.algoDatapoint).toBe(3);
+    expect(dp.exponential).toBe(4);
+    expect(dp.n).toBe(2);
+    expect(dp.n_qubed).toBe(8);
+    expect(dp.n_squared).toBe(4);
+    expect(dp.n_log_n).toBe(2);
+    expect(dp.log_n).toBe(1);
   });
 });
